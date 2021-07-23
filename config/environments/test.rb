db29eaec6ff45d2a8eabc98f18f61250a0a37a3e
Yves Hurdequint <hurdequinty@hotmail.com>
2021-07-23 15:36:38 +0800

    app functionning

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -31,8 +31,8 @@ Rails.application.configure do
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
 
-  # Store uploaded files on the local file system in a temporary directory.
-  config.active_storage.service = :test
+  # config/environments/development.rb
+  config.active_storage.service = :cloudinary
 
   config.action_mailer.perform_caching = false
 
