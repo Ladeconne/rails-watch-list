80ccd76743bacbfe21e0059b8546c292f66bc0bb
Yves Hurdequint <hurdequinty@hotmail.com>
2021-07-23 11:43:42 +0800

    added root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lists, only: %w[index new show create] do
+  root to: 'lists#index'
+  resources :lists, only: %w[new show create] do
     resources :bookmarks, only: %w[new create]
   end
   resources :bookmarks, only: :destroy
